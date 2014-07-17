371a8e9c9195a62fa32924424f69fb8b290ba097
Eder Roger de Souza <eder.roger.souza@gmail.com>
2014-07-17 00:16:26 -0300

    auto deploy web machine with puppet. Enviroment by code . nice :]

--- Vagrantfile
+++ Vagrantfile
@@ -17,15 +17,11 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
 
   config.vm.define :web do |web_config|
     web_config.vm.network :private_network, :ip => "192.168.33.12"
-  end
-
-  config.vm.define :web2 do |web_config|
-    web_config.vm.network :private_network, :ip => "192.168.33.13" 
     web_config.vm.provision "puppet" do |puppet|
       puppet.manifest_file = "web.pp"
     end
   end
-
+  
   config.vm.define :monitor do |monitor_config|
     monitor_config.vm.network :private_network, :ip => "192.168.33.14"
   end
--- manifests/web.pp
+++ manifests/web.pp
@@ -5,3 +5,61 @@ package { ["mysql-client", "tomcat7"]:
  ensure => installed,
  require => Exec["apt-update"],
 }
+
+file { "/var/lib/tomcat7/conf/.keystore":
+ owner => root,
+ group => tomcat7,
+ mode => 0640,
+ source => "/vagrant/manifests/.keystore",
+ require => Package["tomcat7"],
+ notify => Service["tomcat7"],
+}
+
+file { "/var/lib/tomcat7/conf/server.xml":
+ owner => root,
+ group => tomcat7,
+ mode => 0644,
+ source => "/vagrant/manifests/server.xml",
+ require => Package["tomcat7"],
+ notify => Service["tomcat7"],
+}
+
+file { "/etc/default/tomcat7":
+ owner => root,
+ group => root,
+ mode => 0644,
+ source => "/vagrant/manifests/tomcat7",
+ require => Package["tomcat7"],
+ notify => Service["tomcat7"],
+}
+
+service { "tomcat7":
+ ensure => running,
+ enable => true,
+ hasstatus => true,
+ hasrestart => true,
+ require => Package["tomcat7"],
+}
+
+$db_host = "192.168.33.10"
+$db_schema = "loja_schema"
+$db_user = "loja"
+$db_password = "lojasecret"
+
+file { "/var/lib/tomcat7/conf/context.xml":
+ owner => root,
+ group => tomcat7,
+ mode => 0644,
+ content => template("/vagrant/manifests/context.xml"),
+ require => Package["tomcat7"],
+ notify => Service["tomcat7"],
+}
+
+file { "/var/lib/tomcat7/webapps/devopsnapratica.war":
+ owner => tomcat7,
+ group => tomcat7,
+ mode => 0644,
+ source => "/vagrant/manifests/devopsnapratica.war",
+ require => Package["tomcat7"],
+ notify => Service["tomcat7"],
+}
